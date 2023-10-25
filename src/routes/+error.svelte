04c5fa9d66264aa71705c394c60f77d76f50b102
Daniel Matsson <jagardaniel@gmail.com>
2023-10-25 22:03:56 +0200

    Added error message and updated packages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<div class="p-4">
+  <p class="mb-1 text-2xl">Something went wrong!</p>
+  <p class="text-sm">{$page.error?.message} ({$page.status})</p>
+</div>
